530520722e2b20d8baacb5e905df32d05fbd9aed
“lichao” <1017020555@qq.com>
2021-10-05 20:40:27 +0800

    上传

--- src/components/library/EditForm.vue
+++ src/components/library/EditForm.vue
@@ -21,6 +21,7 @@
         </el-form-item>
         <el-form-item label="封面" :label-width="formLabelWidth" prop="cover">
           <el-input v-model="form.cover" autocomplete="off" placeholder="图片 URL"></el-input>
+          <img-upload @onUpload="uploadImg" ref="imgUpload"></img-upload>
         </el-form-item>
         <el-form-item label="简介" :label-width="formLabelWidth" prop="abs">
           <el-input type="textarea" v-model="form.abs" autocomplete="off"></el-input>
@@ -49,8 +50,11 @@
 </template>
 
 <script>
+  import ImgUpload from './ImgUpload'
+
   export default {
     name: 'EditForm',
+    components: {ImgUpload},
     data () {
       return {
         dialogFormVisible: false,
@@ -89,20 +93,24 @@
       },
       onSubmit () {
         this.$axios.post('/books', {
-            id: this.form.id,
-            cover: this.form.cover,
-            title: this.form.title,
-            author: this.form.author,
-            date: this.form.date,
-            press: this.form.press,
-            abs: this.form.abs,
-            category: this.form.category
-          }).then(resp => {
+          id: this.form.id,
+          cover: this.form.cover,
+          title: this.form.title,
+          author: this.form.author,
+          date: this.form.date,
+          press: this.form.press,
+          abs: this.form.abs,
+          category: this.form.category
+        }).then(resp => {
           if (resp && resp.status === 200) {
             this.dialogFormVisible = false
             this.$emit('onSubmit')
           }
         })
+
+      },
+      uploadImg () {
+        this.form.cover=this.$refs.imgUpload.url
       }
     }
   }
